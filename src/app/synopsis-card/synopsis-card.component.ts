cdc72d4154f3aa632e6ee1545efc3d1c488a22b7
obitown <92471740+obitown@users.noreply.github.com>
2022-05-12 17:57:03 -0700

    working synopsisCard

--- src/app/synopsis-card/synopsis-card.component.ts
+++ src/app/synopsis-card/synopsis-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-synopsis-card',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SynopsisCardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    /**
+     * @param data
+     */
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      title: string
+      description: string
+    }
+  ) { }
 
   ngOnInit(): void {
   }
