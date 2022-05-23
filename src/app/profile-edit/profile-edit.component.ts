da236e0553eaebcbfc27be790a964a86c3acf692
obitown <92471740+obitown@users.noreply.github.com>
2022-05-23 14:01:07 -0700

    add editProfile html

--- src/app/profile-edit/profile-edit.component.ts
+++ src/app/profile-edit/profile-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { UserRegistrationService } from '../fetch-api-data.service';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-profile-edit',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileEditComponent implements OnInit {
 
-  constructor() { }
+  @Input() userData: any = {};
+
+  constructor(
+    public fetchApiData: UserRegistrationService,
+    public dialogRef: MatDialogRef<ProfileEditComponent>,
+    public snackBar: MatSnackBar,
+
+  ) { }
 
   ngOnInit(): void {
   }
+
+  editUser(): void {
+    console.log('test')
+  }
+
 
 }
