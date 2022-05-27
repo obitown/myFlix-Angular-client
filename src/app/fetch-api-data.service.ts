d3146987b6a4df7cd8a4f67359973a7bb4ec1d94
obitown <92471740+obitown@users.noreply.github.com>
2022-05-27 11:53:35 -0700

    Update fetch-api-data.service.ts

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -151,6 +151,8 @@ export class UserRegistrationService {
 
   //delete users profile
   deleteUserProfile(): Observable<any> {
+    const token = localStorage.getItem('token');
+    const username = localStorage.getItem('user');
     return this.http.delete(apiUrl + `users/${username}`, {
       headers: new HttpHeaders({
         Authorization: 'Bearer ' + token,
